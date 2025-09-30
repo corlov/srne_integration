58ee706085d01f342c1152b6e574d521bd0ce07f
corlov <corlov@gmail.com>
2025-09-30 09:41:40 +0300

    feat: Add minimal test service to isolate the problem 6

--- web_app/backend/services/test_service/test_app.py
+++ web_app/backend/services/test_service/test_app.py
@@ -16,7 +16,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             response = {
                 "message": "This is the TEST service!",
                 "version": VERSION,
-                "status": "OK 5"
+                "status": "OK 6"
             }
             self.wfile.write(json.dumps(response).encode())
         else:
