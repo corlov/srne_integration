9c781a9613a867f370db86b7815f792816c6bd48
corlov <corlov@gmail.com>
2025-09-30 08:37:22 +0300

    feat: Add minimal test service to isolate the problem

---
+++ web_app/backend/services/test_service/test_app.py
@@ -0,0 +1,30 @@
+
+import http.server
+import socketserver
+import json
+import os
+
+PORT = 8001
+VERSION = os.getenv('APP_VERSION', 'unknown_version')
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/version':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = {
+                "message": "This is the TEST service!",
+                "version": VERSION,
+                "status": "OK"
+            }
+            self.wfile.write(json.dumps(response).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Serving test_service at port {PORT}")
+    print(f"Version: {VERSION}")
+    httpd.serve_forever()
