50255b1ab93443f45037a489ae801bbcc2169896
corlov <corlov@gmail.com>
2025-10-10 11:09:13 +0300

    feat: добавлена автоматизация через Helm 3

--- web_app/backend/services/test_service/test_app.py
+++ web_app/backend/services/test_service/test_app.py
@@ -16,7 +16,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             response = {
                 "message": "This is the TEST service!",
                 "version": VERSION,
-                "status": "OK `10"
+                "status": "OK `11"
             }
             self.wfile.write(json.dumps(response).encode())
         else:
