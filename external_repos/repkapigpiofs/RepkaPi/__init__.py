3b79133c736fa918879d5c3b8c178a84f2269b1c
corlov <corlov@gmail.com>
2025-10-12 13:13:29 +0300

    Рефакторинг структуры проекта и удаление старых файлов

--- external_repos/repkapigpiofs/RepkaPi/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2018 Richard Hull
-# Адаптация и доработка под Repka Pi (c) 2023 Дмитрий Шевцов (@screatorpro)
-# Портирование и доработка под Repka Pi 4 (c) 2025 Семён Платцев 
